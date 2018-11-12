79376090377b320fba86a80ccccc94b213675a5c
yehuda <yuda0527@gmail.com>
2018-11-12 12:20:35 +0200

    delete file

--- src/pages/managmentTools.js
+++ src/pages/managmentTools.js
@@ -1,195 +1,195 @@
-import React, { Component } from 'react';
-import '../App.css';
-import { connect } from 'react-redux'
-import store from '../store/store.js';
-import axios from 'axios';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import PDFpreview  from './pdfPreview';
-import Versions  from './versions.js';
-import PrintButton from '../createPDF/PrintButton.js'  
+// import React, { Component } from 'react';
+// import '../App.css';
+// import { connect } from 'react-redux'
+// import store from '../store/store.js';
+// import axios from 'axios';
+// import { BrowserRouter, Route, Link } from 'react-router-dom';
+// import PDFpreview  from './pdfPreview';
+// import Versions  from './versions.js';
+// import PrintButton from '../createPDF/PrintButton.js'  
 
-import RichEditor from '../richEditor/richEditor.js'
-import {Col, FormGroup, Input, Label, Row} from "reactstrap";
+// import RichEditor from '../richEditor/richEditor.js'
+// import {Col, FormGroup, Input, Label, Row} from "reactstrap";
 
 
 
-class ManagmentTools extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            displayPdf: false
-        }
-    }
-    render() {
+// class ManagmentTools extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             displayPdf: false
+//         }
+//     }
+//     render() {
         
-        return (
-            <div>
-                <h1>Start scoping</h1>
+//         return (
+//             <div>
+//                 <h1>Start scoping</h1>
 
-                <FormGroup>
-                    <Label for="exampleSelect" sm={2}>Select</Label>
-                        <Input type="select" name="select" id="exampleSelect"
-                               defaultValue={this.props.currentProject} onChange={(e) => {
-                            store.dispatch({ type: 'UPDATE_CURRENT_PROJECT_ID', payload: e.target.value });
-                            store.dispatch({ type: 'GET_ALL_DATA' });
-                        }}>
-                        <option value='' style={{ color: 'red' }} >Select project</option>
-                            {this.props.projectsArray.map((elm, i) => {
-                                return <option key={elm._id} value={elm._id}>{elm.projectName}</option>})}
-                        </Input>
-                </FormGroup>
-                {/*<select defaultValue={this.props.currentProject} onChange={(e) => {*/}
-                    {/*store.dispatch({ type: 'UPDATE_CURRENT_PROJECT_ID', payload: e.target.value });*/}
-                    {/*store.dispatch({ type: 'GET_ALL_DATA' });*/}
-                {/*}}>*/}
-                    {/*<option value='' style={{ color: 'red' }} >Select project</option>*/}
-                    {/*{this.props.projectsArray.map((elm, i) => {*/}
-                        {/*return <option key={elm._id} value={elm._id}>{elm.projectName}</option>})}*/}
-                {/*</select>*/}
-                {this.props.currentProject === '' ? <CreateNewProject /> : null}
-                {this.props.currentProject === '' ? null : <ScopingContinuation store={this.props} />}
+//                 <FormGroup>
+//                     <Label for="exampleSelect" sm={2}>Select</Label>
+//                         <Input type="select" name="select" id="exampleSelect"
+//                                defaultValue={this.props.currentProject} onChange={(e) => {
+//                             store.dispatch({ type: 'UPDATE_CURRENT_PROJECT_ID', payload: e.target.value });
+//                             store.dispatch({ type: 'GET_ALL_DATA' });
+//                         }}>
+//                         <option value='' style={{ color: 'red' }} >Select project</option>
+//                             {this.props.projectsArray.map((elm, i) => {
+//                                 return <option key={elm._id} value={elm._id}>{elm.projectName}</option>})}
+//                         </Input>
+//                 </FormGroup>
+//                 {/*<select defaultValue={this.props.currentProject} onChange={(e) => {*/}
+//                     {/*store.dispatch({ type: 'UPDATE_CURRENT_PROJECT_ID', payload: e.target.value });*/}
+//                     {/*store.dispatch({ type: 'GET_ALL_DATA' });*/}
+//                 {/*}}>*/}
+//                     {/*<option value='' style={{ color: 'red' }} >Select project</option>*/}
+//                     {/*{this.props.projectsArray.map((elm, i) => {*/}
+//                         {/*return <option key={elm._id} value={elm._id}>{elm.projectName}</option>})}*/}
+//                 {/*</select>*/}
+//                 {this.props.currentProject === '' ? <CreateNewProject /> : null}
+//                 {this.props.currentProject === '' ? null : <ScopingContinuation store={this.props} />}
 
-            </div>
-        );
-    }
-}
+//             </div>
+//         );
+//     }
+// }
 
-class CreateNewProject extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            projectName: "",
-            editorName: "",
-        }
-    }
+// class CreateNewProject extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             projectName: "",
+//             editorName: "",
+//         }
+//     }
 
-    saveBtn = () =>{
-        var inputEmpty = this.state.projectName === '' || this.state.editorName === '';
-       return <button className={inputEmpty ? 'btn btn-secondary disableBtn': 'btn btn-primary'} onClick={() => {
-           store.dispatch({type: 'CREATE_NEW_PROJECT', payload: this.state})
-            {/* this.createNewProject(this.state) */}
-            this.setState({ projectName: "",editorName: "",})
-            }}><Link to='/scoping' >Create New Project</Link></button>
-    }
-    render() {
-        return (
-            <div className='newProject'>
+//     saveBtn = () =>{
+//         var inputEmpty = this.state.projectName === '' || this.state.editorName === '';
+//        return <button className={inputEmpty ? 'btn btn-secondary disableBtn': 'btn btn-primary'} onClick={() => {
+//            store.dispatch({type: 'CREATE_NEW_PROJECT', payload: this.state})
+//             {/* this.createNewProject(this.state) */}
+//             this.setState({ projectName: "",editorName: "",})
+//             }}><Link to='/scoping' >Create New Project</Link></button>
+//     }
+//     render() {
+//         return (
+//             <div className='newProject'>
 
-                {/*<input type="text" placeholder='ProjectName' value={this.state.projectName} onChange={(e) => {*/}
-                    {/*this.setState({ projectName: e.target.value })*/}
-                {/*}} />*/}
-                {/*<input type="text" placeholder='Editor name' value={this.state.editorName} onChange={(e) => {*/}
-                    {/*this.setState({ editorName: e.target.value })*/}
-                {/*}} />*/}
-                <Row form>
-                    <Col md={6}>
-                        <FormGroup>
-                            <Label for="ProjectName">Project Name</Label>
-                            <Input type="text" name="ProjectName" id="ProjectName"
-                                   value={this.state.projectName} onChange={(e) => {
-                                this.setState({ projectName: e.target.value })
-                            }} />
-                        </FormGroup>
-                    </Col>
-                    <Col md={6}>
-                        <FormGroup>
-                            <Label for="Editor name">Editor name</Label>
-                            <Input type="text" name="EditorName" id="EditorName"
-                                   value={this.state.editorName} onChange={(e) => {
-                                this.setState({ editorName: e.target.value })
-                            }} />
-                        </FormGroup>
-                    </Col>
-                </Row>
-                {this.saveBtn()}
-            </div>
-        );
-    }
-}
+//                 {/*<input type="text" placeholder='ProjectName' value={this.state.projectName} onChange={(e) => {*/}
+//                     {/*this.setState({ projectName: e.target.value })*/}
+//                 {/*}} />*/}
+//                 {/*<input type="text" placeholder='Editor name' value={this.state.editorName} onChange={(e) => {*/}
+//                     {/*this.setState({ editorName: e.target.value })*/}
+//                 {/*}} />*/}
+//                 <Row form>
+//                     <Col md={6}>
+//                         <FormGroup>
+//                             <Label for="ProjectName">Project Name</Label>
+//                             <Input type="text" name="ProjectName" id="ProjectName"
+//                                    value={this.state.projectName} onChange={(e) => {
+//                                 this.setState({ projectName: e.target.value })
+//                             }} />
+//                         </FormGroup>
+//                     </Col>
+//                     <Col md={6}>
+//                         <FormGroup>
+//                             <Label for="Editor name">Editor name</Label>
+//                             <Input type="text" name="EditorName" id="EditorName"
+//                                    value={this.state.editorName} onChange={(e) => {
+//                                 this.setState({ editorName: e.target.value })
+//                             }} />
+//                         </FormGroup>
+//                     </Col>
+//                 </Row>
+//                 {this.saveBtn()}
+//             </div>
+//         );
+//     }
+// }
 
-class ScopingContinuation extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            newVersionMode: false,
-            displayPdf: false,
-            displayVersions: false
-        }
-    }
-    cancelNewVersionMode = () => {
-        this.setState({ newVersionMode: false })
-    }
-    render() {
+// class ScopingContinuation extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             newVersionMode: false,
+//             displayPdf: false,
+//             displayVersions: false
+//         }
+//     }
+//     cancelNewVersionMode = () => {
+//         this.setState({ newVersionMode: false })
+//     }
+//     render() {
 
-        return (
-            <div>
+//         return (
+//             <div>
                 
-                {this.state.newVersionMode ? null : <button><Link to='/scoping' >Continue Scoping</Link></button>}
-                {this.state.newVersionMode ? null : <button onClick={() => this.setState({ newVersionMode: true })}>New version</button>}
-                {this.state.newVersionMode ? <CreateNewVersion cancelNewVersionMode={this.cancelNewVersionMode} /> : null}
-                <button onClick={() => this.setState({ displayPdf: !this.state.displayPdf })}>view all data</button>
-                <button onClick={() => this.setState({ displayVersions: !this.state.displayVersions })}>view all versions</button>
-                {this.state.displayVersions?  <Versions/>: null}
-                {this.state.displayPdf ? 
-                <div>
+//                 {this.state.newVersionMode ? null : <button><Link to='/scoping' >Continue Scoping</Link></button>}
+//                 {this.state.newVersionMode ? null : <button onClick={() => this.setState({ newVersionMode: true })}>New version</button>}
+//                 {this.state.newVersionMode ? <CreateNewVersion cancelNewVersionMode={this.cancelNewVersionMode} /> : null}
+//                 <button onClick={() => this.setState({ displayPdf: !this.state.displayPdf })}>view all data</button>
+//                 <button onClick={() => this.setState({ displayVersions: !this.state.displayVersions })}>view all versions</button>
+//                 {this.state.displayVersions?  <Versions/>: null}
+//                 {this.state.displayPdf ? 
+//                 <div>
                  
 
 
-                    <PrintButton  id={"pdfPreview"} label={"Create PDF file"} />
-                    <PDFpreview />
+//                     <PrintButton  id={"pdfPreview"} label={"Create PDF file"} />
+//                     <PDFpreview />
 
                     
-                </div>
-                 : null}
+//                 </div>
+//                  : null}
 
 
-            </div>
-        );
-    }
-}
-
-
-
-class CreateNewVersion extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            rejectionStatus: true,
-            editorNameStatus: false,
-            rejectionExplenation: "",
-            editorName: "",
-        }
-    }
-
-    render() {
-        return (
-            <div className='newVersion'>
-                {
-                    this.state.rejectionStatus ? <div>
-                        <textarea cols="30" rows="10" placeholder='Rejection explenation' onChange={e => { this.setState({ rejectionExplenation: e.target.value }) }}></textarea>
-                        <br />
-                        <button onClick={this.props.cancelNewVersionMode}>Cancel</button>
-                        <button className={this.state.rejectionExplenation  === '' ? 'disableBtn': 'saveBtn'} onClick={() => {
-                            this.setState({ rejectionStatus: false, editorNameStatus: true })
-                            store.dispatch({ type: 'REJECTION_EXPLENATION', payload: this.state.rejectionExplenation })
-                        }}>Save rejection explenation</button>
-                    </div>
-                        : null}
-                {
-                    this.state.editorNameStatus ? <div>
-                        <input type="text" placeholder='Editor name' onChange={e => { this.setState({ editorName: e.target.value }) }} />
-                        <button className={this.state.editorName === '' ? 'disableBtn': 'saveBtn'} onClick={() => {
-                            this.props.cancelNewVersionMode()
-                            this.setState({editorNameStatus: false })
-                            store.dispatch({ type: 'CREATE_NEW_VERSION', payload: this.state.editorName })
-                            }}><Link to='/scoping' >Create New Version</Link></button>
-                    </div>
-                        : null}
-            </div>
-        );
-    }
-}
+//             </div>
+//         );
+//     }
+// }
 
 
 
-export default connect(store => store)(ManagmentTools);
+// class CreateNewVersion extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             rejectionStatus: true,
+//             editorNameStatus: false,
+//             rejectionExplenation: "",
+//             editorName: "",
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <div className='newVersion'>
+//                 {
+//                     this.state.rejectionStatus ? <div>
+//                         <textarea cols="30" rows="10" placeholder='Rejection explenation' onChange={e => { this.setState({ rejectionExplenation: e.target.value }) }}></textarea>
+//                         <br />
+//                         <button onClick={this.props.cancelNewVersionMode}>Cancel</button>
+//                         <button className={this.state.rejectionExplenation  === '' ? 'disableBtn': 'saveBtn'} onClick={() => {
+//                             this.setState({ rejectionStatus: false, editorNameStatus: true })
+//                             store.dispatch({ type: 'REJECTION_EXPLENATION', payload: this.state.rejectionExplenation })
+//                         }}>Save rejection explenation</button>
+//                     </div>
+//                         : null}
+//                 {
+//                     this.state.editorNameStatus ? <div>
+//                         <input type="text" placeholder='Editor name' onChange={e => { this.setState({ editorName: e.target.value }) }} />
+//                         <button className={this.state.editorName === '' ? 'disableBtn': 'saveBtn'} onClick={() => {
+//                             this.props.cancelNewVersionMode()
+//                             this.setState({editorNameStatus: false })
+//                             store.dispatch({ type: 'CREATE_NEW_VERSION', payload: this.state.editorName })
+//                             }}><Link to='/scoping' >Create New Version</Link></button>
+//                     </div>
+//                         : null}
+//             </div>
+//         );
+//     }
+// }
+
+
+
+// export default connect(store => store)(ManagmentTools);
